bc9149b813b293e08550f6afd88fb13d3a48e6da
Hiroshi Urabe <urabe@ham.works>
2020-01-18 13:23:15 +0900

    get container url

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
-env
 
 pip3 install -r ./shifter/requirements.txt
 
-python3 ./shifter/run.py start
+RESULT=$(python3 ./shifter/run.py start)
+regexp="::set-output name=shifter_app_url::(https://[0-9a-zA-Z?=#+-_&:/.%]*\.io:[0-9]+)"
+[[ ${RESULT} =~ $regexp ]]
+export CONTAINER_URL=${BASH_REMATCH[1]}
 npm run build
 python ./shifter/run.py stop
